f41c3800cda7ee5d39be24567ef2cdc2132685e1
Juli-29 <57229140+Juli-29@users.noreply.github.com>
2020-03-15 15:47:56 +0300

    Create double_vector.cpp

---
+++ double_vector.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<vector<int>>stuff;
+	for (int i = 0; i < 5; i++)
+	{
+		vector<int>temp;
+		for (int j = 0; j < 5; j++)
+		{	
+			temp.push_back(i);
+		}
+		stuff.push_back(temp);
+	}
+	for (int i = 0; i < stuff.size(); i++)
+	{
+		for (int j = 0; j < stuff[i].size(); j++)
+		{
+			cout << stuff[i][j];
+		}
+		cout << endl;
+	}
+}
